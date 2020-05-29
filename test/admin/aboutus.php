0212cd3bf8d9c8ca3aabc53568951aaa57a41963
1233448 <66132639+1233448@users.noreply.github.com>
2020-05-29 23:27:58 +0530

    my todo website

---
+++ test/admin/aboutus.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <title>About</title>
+<link rel="stylesheet" href="../csss/aboutus.css" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Flamenco" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+
+</head>
+<body>
+    <header>
+      <nav>
+        <div class="row clearfix">
+          <img src="../image/logo_1.png" class="logo"/>
+            <ul class="main-nav" animate slideInDown>
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="aboutus.php">About Us</a></li>
+                <li><a href="contactus.php">Contact Us</a></li>
+                <li><a href="../login.php">Admin Login</a></li>
+          </ul>
+        </div>
+      </nav>
+      <div class="main-content-header">
+          <h2 class="search">About Us</h2>
+             <div class="div2">
+             <h2>Introduction :</h2>
+             <p>My Name is Satendra Sahani From Bhatni Bujurg and noe in this Present time i am live in Noida secter 15 <br> You can contact us mobile no 8573890224 Email id :satendrasahani19@gmail.com and You can search on youtuv PHP TUTORIAL IN HINDI <br>and you can search SS Hitech our blog  Satendrasahani.blogspot.com</p>
+             </div>  
+          </div>
+    </header>
+    
+</body>
+</html>
